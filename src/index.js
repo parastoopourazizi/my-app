c7c12215452593595f9108cf6023bbcec009e608
parastoopourazizi <napourazizi@gmail.com>
2023-08-15 13:58:40 +0330

    add card component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import Card from './Card'
+
 
 
 
@@ -13,36 +15,10 @@ root.render(
 <header>header</header>
 <section>
   <div className="row">
-    <div className="col">
-      <div className="card">
-        <img src="./images/1.jpg" alt="" />
-        <div className="card-body">
-          <h5 className="card-title">shoes1</h5>
-          <p className="card-text">Men's Sneakers Running Shoes Tennis Shoes Comfort Shoes Casual Outdoor Daily Walking Shoes PU Black / Red White Dark Blue Fall</p>
-          <button className='add-to-card'>add to card</button>
-        </div>
-      </div>
-    </div>
-    <div className="col">
-      <div className="card">
-        <img src="./images/1.jpg" alt="" />
-        <div className="card-body">
-          <h5 className="card-title">shoes1</h5>
-          <p className="card-text">Men's Sneakers Running Shoes Tennis Shoes Comfort Shoes Casual Outdoor Daily Walking Shoes PU Black / Red White Dark Blue Fall</p>
-          <button className='add-to-card'>add to card</button>
-        </div>
-      </div>
-    </div>
-    <div className="col">
-      <div className="card">
-        <img src="./images/1.jpg" alt="" />
-        <div className="card-body">
-          <h5 className="card-title">shoes1</h5>
-          <p className="card-text">Men's Sneakers Running Shoes Tennis Shoes Comfort Shoes Casual Outdoor Daily Walking Shoes PU Black / Red White Dark Blue Fall</p>
-           <button className='add-to-card'>add to card</button>
-        </div>
-      </div>
-    </div>
+    <Card />
+    <Card />
+    <Card />
+    
   </div>
 </section>
 <footer>footer
