9cc0297046f684d0085dcb72a219c7e77f24c02d
parastoopourazizi <napourazizi@gmail.com>
2023-08-29 13:18:22 +0330

    complete post component

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,9 +1,13 @@
-import './Blog.css'
-import BlogItem from './BlogItem'
+import './Blog.css';
+import BlogItem from './BlogItem';
+import blog1image from '../../assets/images/post-1.jpg'
+import blog2image from '../../assets/images/post-2.jpg'
+import blog3image from '../../assets/images/post-3.jpg'
+
 function Blog(){
     let BlogItems = [
         {
-            BlogImg :'',
+            BlogImg :blog1image,
             BlogTitle : 'Post title1',
             BlogText : '1-Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',
             BlogAuthor : 'parastoo pourazizi',
@@ -11,7 +15,7 @@ function Blog(){
 
         },
         {
-            BlogImg  :'',
+            BlogImg  :blog2image,
             BlogTitle : 'Post title2',
             BlogText : '2-Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',
             BlogAuthor : 'narges pourazizi',
@@ -19,7 +23,7 @@ function Blog(){
 
         },
         {
-            BlogImg  :'',
+            BlogImg  :blog3image,
             BlogTitle : 'Post title3',
             BlogText : '3-Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',
             BlogAuthor : 'parastoo pourazizi',
--- src/components/blog/BlogItem.js
+++ src/components/blog/BlogItem.js
@@ -5,7 +5,7 @@ function BlogItem({BlogImg ,BlogTitle , BlogText , BlogAuthor ,  BlogTime}){
         <div className="col-4">
             <div className="blog-box">
               <div className="blog-img">
-                <img src="./img/post-1.jpg" className="img-fluid" />
+                <img src={BlogImg} className="img-fluid" />
               </div>
               <div className="blog-content">
                 <h3 className="blog-title">{BlogTitle}</h3>
@@ -18,7 +18,7 @@ function BlogItem({BlogImg ,BlogTitle , BlogText , BlogAuthor ,  BlogTime}){
                   <p className="author">{BlogAuthor}</p>
                 </div>
                 <div className="post-date">
-                  <span className="bi bi-clock"></span> {BlogTime}
+                  <span className="bi bi-clock"></span> {BlogTime} min
                 </div>
               </div>
             </div>
